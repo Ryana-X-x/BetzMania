17923d7b6889ecc084ffdf4e6dd630835a62d9d1
Shad0wcoder <161946619+Shad0wcoder@users.noreply.github.com>
2024-11-12 18:02:47 +0530

    stake change

--- app/stake/page.jsx
+++ app/stake/page.jsx
@@ -1,18 +1,14 @@
 "use client"
-import dynamic from "next/dynamic";
-import styles from '../../styles/st.module.css'
-
-const BettingGame = dynamic(() => import("@/components/BettingGame"), {
-  ssr: false,
-})
+import BettingGame from "@/components/BettingGame";
+import styles from '../../styles/st.module.css';
 
 export default function Home() {
   return (
     <div className={styles.con1}>
-    <main className="flex min-h-screen flex-col items-center pt-10">
-      <h1 className="text-4xl font-bold mb-8"> Welcome to Mine Betting Game</h1>
-      <BettingGame />
-    </main>
+      <main className="flex min-h-screen flex-col items-center pt-10">
+        <h1 className="text-4xl font-bold mb-8"> Welcome to Mine Betting Game</h1>
+        <BettingGame />
+      </main>
     </div>
-  )
+  );
 }
